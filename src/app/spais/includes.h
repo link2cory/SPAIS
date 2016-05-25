02565ffc83a3309b1524a730f1ade77a25210d33
Cory Perkins <Link2cory+farfor_github@gmail.com>
2016-05-25 14:24:46 -0700

    added master includes file.  This was missing from the last commit

---
+++ src/app/spais/includes.h
@@ -0,0 +1,26 @@
+#ifndef SPAIS_MASTER_INCLUDES_CFG_H
+#define SPAIS_MASTER_INCLUDES_CFG_H
+
+// General Purpose Includes
+#include "dn_common.h"
+#include "string.h"
+#include "stdio.h"
+#include "cli_task.h"
+#include "loc_task.h"
+#include "dn_system.h"
+#include "dn_gpio.h"
+#include "dnm_local.h"
+#include "dn_fs.h"
+#include "dn_adc.h"
+#include "dn_exe_hdr.h"
+#include "well_known_ports.h"
+#include "Ver.h"
+#include "dn_typedef.h"
+
+// SPAIS specific
+#include "main_cfg.h"
+#include "moisture_sense_cfg.h"
+#include "data_send_cfg.h"
+#include "valve_control_cfg.h"
+
+#endif
